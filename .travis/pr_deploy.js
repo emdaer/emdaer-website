05766f56001b3d0fb2d30172828577ef3b84893b
Luke Herrington <lherrington100@gmail.com>
2017-11-10 12:43:30 -0800

    ci: only deploy pr review site if change to website occurred

--- .travis/pr_deploy.js
+++ .travis/pr_deploy.js
@@ -6,7 +6,15 @@ const exec = util.promisify(require('child_process').exec);
 
 async function deploy() {
   if (!process.env.TRAVIS_PULL_REQUEST) {
-    Promise.reject('Skipping PR deploy. Not a PR build.');
+    return Promise.reject('Skipping PR deploy. Not a PR build.');
+  }
+  const { stdout: gitDiff } = await exec(
+    'git diff --name-only $TRAVIS_COMMIT_RANGE src yarn.lock package.json gatsby-config.js'
+  );
+  if (gitDiff.length === 0) {
+    return Promise.reject(
+      'Skipping PR deploy. No changes made to website source'
+    );
   }
   const result = await deployOnce({
     GH_TOKEN: process.env.GH_TOKEN,
@@ -16,23 +24,24 @@ async function deploy() {
     try {
       await exec('make build');
       process.chdir('public');
-      const out = await exec('../node_modules/.bin/surge-review');
-      stdout = out.stdout;
-      stderr = out.stderr;
+      await exec('../node_modules/.bin/surge-review');
     } catch (e) {
-      Promise.reject(e);
+      return Promise.reject(e);
     }
-    stdout && console.log('stdout:', stdout);
-    stderr && console.log('stderr:', stderr);
+    return Promise.resolve('Successfully deployed PR site');
   } else if (result === false) {
-    Promise.reject('Some job(s) failed');
+    return Promise.reject('Some job(s) failed');
   } else if (result === null) {
-    Promise.reject('Did not run as the build leader');
+    return Promise.reject('Did not run as the build leader');
   }
 }
 
-try {
-  deploy();
-} catch (e) {
-  console.error(e);
-}
+module.exports = deploy;
+
+(async () => {
+  try {
+    await module.exports();
+  } catch (e) {
+    console.error(e);
+  }
+})();
