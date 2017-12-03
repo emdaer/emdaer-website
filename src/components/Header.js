6ab2ff408be407535f048f74a7acd423362de5d4
flipactual <flipactual@gmail.com>
2017-12-02 20:30:07 -0600

    fix: use withTheme for brand color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import g from 'glamorous';
+import g, { withTheme } from 'glamorous';
 import { rhythm } from '../utils/typography';
 import Menu from './Menu';
 import RibbonContainer from './RibbonContainer';
 
-export default () => (
+export default withTheme(({ theme: { brand } }) => (
   <RibbonContainer
     color="white"
-    backgroundColor="#E25E3E"
+    backgroundColor={brand}
     padding={`${rhythm(1 / 2)} ${rhythm(3 / 4)}`}
   >
     <g.Div display="flex" justifyContent="center">
       <Menu />
     </g.Div>
   </RibbonContainer>
-);
+));
